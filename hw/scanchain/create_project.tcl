70748386ae9cf561433b4008b96e3e20798b6116
Daniel Lovell <lovell.daniel92@gmail.com>
2023-02-25 21:48:22 -0800

    Remove sim_compile_state

--- hw/scanchain/create_project.tcl
+++ hw/scanchain/create_project.tcl
@@ -173,7 +173,6 @@ set_property -name "revised_directory_structure" -value "1" -objects $obj
 set_property -name "sim.central_dir" -value "$proj_dir/${_xil_proj_name_}.ip_user_files" -objects $obj
 set_property -name "sim.ip.auto_export_scripts" -value "1" -objects $obj
 set_property -name "simulator_language" -value "Mixed" -objects $obj
-set_property -name "sim_compile_state" -value "1" -objects $obj
 set_property -name "source_mgmt_mode" -value "DisplayOnly" -objects $obj
 set_property -name "webtalk.xsim_launch_sim" -value "4" -objects $obj
 
