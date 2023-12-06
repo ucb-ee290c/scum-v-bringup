d6cacc687bdbbf794926aee8158692a5556ff5bb
Titan Yuan <tryuan99@gmail.com>
2023-12-06 12:00:31 -0800

    Change V22 to V23

--- sw/scum_firmware/core/src/main.c
+++ sw/scum_firmware/core/src/main.c
@@ -195,7 +195,7 @@ int main() {
 
   HAL_UART_init(UART0, &UART_init_config);
   print_baseband_status0();
-  sprintf(str, "SCuM-V22 says, 'I'm alive!'\r\n");
+  sprintf(str, "SCuM-V23 says, 'I'm alive!'\r\n");
   HAL_UART_transmit(UART0, (uint8_t *)str, strlen(str), 0);
 
   // Set scum-v tuning registers
