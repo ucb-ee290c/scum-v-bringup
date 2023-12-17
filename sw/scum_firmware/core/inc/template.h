ea66656496275ffdd996f61532d26ebe8bcf4691
Titan Yuan <tryuan99@gmail.com>
2023-12-16 20:03:09 -0800

    Add template project

---
+++ sw/scum_firmware/core/inc/template.h
@@ -0,0 +1,23 @@
+
+#ifndef __TEMPLATE_H
+#define __TEMPLATE_H
+
+#ifdef __cplusplus
+extern "C" {
+#endif
+
+#include <stdint.h>
+#include <stdio.h>
+#include <string.h>
+
+#include "baseband.h"
+#include "scum_hal.h"
+#include "scum_system.h"
+
+int main();
+
+#ifdef __cplusplus
+}
+#endif
+
+#endif // __TEMPLATE_H
