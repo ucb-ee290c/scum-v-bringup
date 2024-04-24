1e26ee98d54c346181a604442f20f840db3a3cfc
Daniel Lovell <dlovell98@eecs.berkeley.edu>
2024-04-23 18:39:18 -0700

    Updated parse_counter_logs

--- sw/scripts/matlab/parse_counter_logs.m
+++ sw/scripts/matlab/parse_counter_logs.m
@@ -1,5 +1,5 @@
 % Specify the file name (adjust the path as necessary)
-filename = '..\python\logs\counter_regs_100m_25m_dcoff_gnd_idac_16.txt';
+filename = '..\python\logs\counter_regs_100m_25m_150mv_p_offset_idac_16.txt';
 % filename = '..\python\logs\counter_regs_100m_25m_25mv_n_offset_idac_16.txt';
 % Open the file for reading
 fid = fopen(filename, 'rt');
@@ -11,7 +11,7 @@ end
 counter_p = [];
 counter_n = [];
 
-N = 512;
+N = 128;
 % Read the file line by line
 n = 1;
 while n <= N
@@ -64,8 +64,8 @@ for i = 1:N
     % if(dn < lastdn/2)
     %     dn = dn*3;
     % end
-    diff_p(i) = dp * 25e6;
-    diff_n(i) = dn * 25e6;
+    diff_p(i) = dp * 20e6;
+    diff_n(i) = dn * 20e6;
     lastdp = dp;
     lastdn = dn;
 
@@ -74,9 +74,13 @@ end
 diff_p = mod(diff(counter_p), 63) * 25e6;
 diff_n = mod(diff(counter_n), 63) * 25e6;
 
+diff_pm = movmean(diff_p, 8);
+diff_nm = movmean(diff_n, 8);
+
+
 % Plot the raw counter data
-figure;
-subplot(2,1,1);
+figure(1);
+subplot(3,1,1);
 plot(counter_p, '-o');
 hold on;
 plot(counter_n, '-o');
@@ -87,7 +91,7 @@ ylabel('Value');
 legend('CNT_P', 'CNT_N');
 
 % Plot the differences
-subplot(2,1,2);
+subplot(3,1,2);
 plot(diff_p, '-o');
 hold on;
 plot(diff_n, '-o');
@@ -95,5 +99,17 @@ hold off;
 title('Differences in Counter Data');
 xlabel('Sample');
 ylabel('Difference');
+legend('Diff CNT_P', 'Diff CNT_N');
+
+
+% Plot the differences
+subplot(3,1,3);
+plot(diff_pm);
+hold on;
+plot(diff_nm);
+hold off;
+title('Moving Average Meaned Frequency');
+xlabel('Sample');
+ylabel('Meaned Difference');
 legend('Diff CNT_P', 'Diff CNT_N');
 
